aaf27187b4242cdbdb3593783b00b6bd91bbc454
but-sergey <but-sergey@bk.ru>
2022-03-26 15:48:41 +0300

    Nullable ref-types

--- Chapter02/NullHandling/Program.cs
+++ Chapter02/NullHandling/Program.cs
@@ -1,7 +1,17 @@
 ﻿using System;
 
+#nullable enable
+
 namespace NullHandling
 {
+    class Address
+    {
+        public string? Building;
+        public string Street = string.Empty;
+        public string City = string.Empty;
+        public string Region = string.Empty;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -16,6 +26,12 @@ namespace NullHandling
             thisCouldBeNull = 7;
             Console.WriteLine(thisCouldBeNull);
             Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+            var address = new Address();
+            address.Building = null;
+            address.Street = null;
+            address.City = "London";
+            address.Region = null;
         }
     }
 }
