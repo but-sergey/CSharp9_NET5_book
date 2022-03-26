f7d99bc588a433e856fb621e26e46acf72cf2422
but-sergey <but-sergey@bk.ru>
2022-03-26 15:04:59 +0300

    dec, bin and hex notation

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -13,6 +13,13 @@ namespace Numbers
             float realNumber = 2.3f;
 
             double anotherRealNumber = 2.3;
+
+            int decimalNotation = 2_000_000;
+            int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+            int hexadecimalNotation = 0x_001E_8480;
+
+            Console.WriteLine($"{decimalNotation == binaryNotation}");
+            Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
         }
     }
 }
