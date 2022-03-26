a79d45bc02f7edcf3da028778721f1fd3fc533dd
but-sergey <but-sergey@bk.ru>
2022-03-26 15:16:49 +0300

    Doubles and decimals

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -24,6 +24,34 @@ namespace Numbers
             Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
             Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}.");
             Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
+
+            Console.WriteLine("Use doubles:");
+
+            double a = 0.1;
+            double b = 0.2;
+
+            if (a + b == 0.3)
+            {
+                Console.WriteLine($"{a} + {b} equals 0.3");
+            }
+            else
+            {
+                Console.WriteLine($"{a} + {b} does NOT equals 0.3");
+            }
+
+            Console.WriteLine("Use decimals:");
+
+            decimal c = 0.1M;
+            decimal d = 0.2M;
+
+            if (c + d == 0.3M)
+            {
+                Console.WriteLine($"{a} + {b} equals 0.3");
+            }
+            else
+            {
+                Console.WriteLine($"{a} + {b} does NOT equals 0.3");
+            }
         }
     }
 }
