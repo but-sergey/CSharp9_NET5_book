97edf6dcb04ace436e382e818fb74265ad4f7475
but-sergey <but-sergey@bk.ru>
2022-03-26 15:10:03 +0300

    Numbers sizes

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -20,6 +20,10 @@ namespace Numbers
 
             Console.WriteLine($"{decimalNotation == binaryNotation}");
             Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+
+            Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
+            Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}.");
+            Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
         }
     }
 }
