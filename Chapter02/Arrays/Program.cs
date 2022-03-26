1e6705783156dbea0d43547f82bb9cb30b903670
but-sergey <but-sergey@bk.ru>
2022-03-26 15:35:46 +0300

    Simple string array

--- Chapter02/Arrays/Program.cs
+++ Chapter02/Arrays/Program.cs
@@ -6,7 +6,19 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //string[] names;
+
+            string[] names = new string[4];
+
+            names[0] = "Kate";
+            names[1] = "Jack";
+            names[2] = "Rebecca";
+            names[3] = "Tom";
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
         }
     }
 }
