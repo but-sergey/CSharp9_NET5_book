7b2bb8c0b61e9ae8edc3b180b75df03c3f6b06b8
but-sergey <but-sergey@bk.ru>
2022-03-27 20:26:49 +0300

    Boolean operators: and, or, xor

--- Chapter03/BooleanOperators/Program.cs
+++ Chapter03/BooleanOperators/Program.cs
@@ -6,7 +6,21 @@ namespace BooleanOperators
     {
         static void Main(string[] args)
         {
-            
+            bool a = true;
+            bool b = false;
+
+            Console.WriteLine($"AND   | a     | b     ");
+            Console.WriteLine($"a     | {a & a,-5} | {a & b,-5} ");
+            Console.WriteLine($"b     | {b & a,-5} | {b & b,-5} ");
+            Console.WriteLine();
+            Console.WriteLine($"OR    | a     | b     ");
+            Console.WriteLine($"a     | {a | a,-5} | {a | b,-5} ");
+            Console.WriteLine($"b     | {b | a,-5} | {b | b,-5} ");
+            Console.WriteLine();
+            Console.WriteLine($"XOR   | a     | b     ");
+            Console.WriteLine($"a     | {a ^ a,-5} | {a ^ b,-5} ");
+            Console.WriteLine($"b     | {b ^ a,-5} | {b ^ b,-5} ");
+            Console.WriteLine();
         }
     }
 }
