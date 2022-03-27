35921a5f33a3a20ca8a201b6f22eefbcc6be4e9d
but-sergey <but-sergey@bk.ru>
2022-03-27 20:31:21 +0300

    & and && operator

--- Chapter03/BooleanOperators/Program.cs
+++ Chapter03/BooleanOperators/Program.cs
@@ -6,9 +6,10 @@ namespace BooleanOperators
     {
         static void Main(string[] args)
         {
+            
             bool a = true;
             bool b = false;
-
+            /*
             Console.WriteLine($"AND   | a     | b     ");
             Console.WriteLine($"a     | {a & a,-5} | {a & b,-5} ");
             Console.WriteLine($"b     | {b & a,-5} | {b & b,-5} ");
@@ -21,6 +22,19 @@ namespace BooleanOperators
             Console.WriteLine($"a     | {a ^ a,-5} | {a ^ b,-5} ");
             Console.WriteLine($"b     | {b ^ a,-5} | {b ^ b,-5} ");
             Console.WriteLine();
+            */
+
+            Console.WriteLine($"a & DoStuff() = {a & DoStuff()}");
+            Console.WriteLine($"b & DoStuff() = {b & DoStuff()}");
+            Console.WriteLine();
+            Console.WriteLine($"a && DoStuff() = {a && DoStuff()}");
+            Console.WriteLine($"b && DoStuff() = {b && DoStuff()}");
+        }
+
+        private static bool DoStuff()
+        {
+            Console.WriteLine("I am doing some stuff.");
+            return true;
         }
     }
 }
