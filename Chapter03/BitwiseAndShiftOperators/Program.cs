54abb6460fb4434973a394af95fb32f1934285bb
but-sergey <but-sergey@bk.ru>
2022-03-27 20:38:52 +0300

    Bitwise and shift operators

--- Chapter03/BitwiseAndShiftOperators/Program.cs
+++ Chapter03/BitwiseAndShiftOperators/Program.cs
@@ -6,7 +6,18 @@ namespace BitwiseAndShiftOperators
     {
         static void Main(string[] args)
         {
-            
+            int a = 10; // 0000 1010
+            int b = 6;  // 0000 0110
+
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+            Console.WriteLine($"a & b = {a & b}");
+            Console.WriteLine($"a | b = {a | b}");
+            Console.WriteLine($"a ^ b = {a ^ b}");
+
+            Console.WriteLine($"a << 3 = {a << 3}");
+            Console.WriteLine($"a * 8 = {a * 8}");
+            Console.WriteLine($"b >> 1 = {b >> 1}");
         }
     }
 }
