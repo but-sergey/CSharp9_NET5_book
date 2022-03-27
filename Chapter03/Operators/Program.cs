406b51a9feb2a31061eb9fe60358d647a6ec1a0f
but-sergey <but-sergey@bk.ru>
2022-03-27 20:02:55 +0300

    Some arifmetic operations

--- Chapter03/Operators/Program.cs
+++ Chapter03/Operators/Program.cs
@@ -6,6 +6,7 @@ namespace Operators
     {
         static void Main(string[] args)
         {
+            /*
             int a = 3;
             int b = a++;
             Console.WriteLine($"a is {a}, b is {b}");
@@ -13,6 +14,20 @@ namespace Operators
             int c = 3;
             int d = ++c;
             Console.WriteLine($"c is {c}, d is {d}");
+            */
+
+            int e = 11;
+            int f = 3;
+            Console.WriteLine($"e is {e}, f is {f}");
+            Console.WriteLine($"e + f = {e + f}");
+            Console.WriteLine($"e - f = {e - f}");
+            Console.WriteLine($"e * f = {e * f}");
+            Console.WriteLine($"e / f = {e / f}");
+            Console.WriteLine($"e % f = {e % f}");
+
+            double g = 11.0;
+            Console.WriteLine($"g is {g:N1}, f is {f}");
+            Console.WriteLine($"g / f = {g / f}");
         }
     }
 }
