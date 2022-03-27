61b538754c0506c0a42ed740a07da79efb76a1ed
but-sergey <but-sergey@bk.ru>
2022-03-27 19:56:58 +0300

    Prefix and postfix increment

--- Chapter03/Operators/Program.cs
+++ Chapter03/Operators/Program.cs
@@ -6,7 +6,13 @@ namespace Operators
     {
         static void Main(string[] args)
         {
-            
+            int a = 3;
+            int b = a++;
+            Console.WriteLine($"a is {a}, b is {b}");
+
+            int c = 3;
+            int d = ++c;
+            Console.WriteLine($"c is {c}, d is {d}");
         }
     }
 }
