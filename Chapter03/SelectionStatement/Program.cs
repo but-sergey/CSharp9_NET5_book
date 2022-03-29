86cc3d620cab15aec44a931d7c3f3c08ae937c26
but-sergey <but-sergey@bk.ru>
2022-03-29 14:28:44 +0300

    Pattern  matching with if-statement

--- Chapter03/SelectionStatement/Program.cs
+++ Chapter03/SelectionStatement/Program.cs
@@ -6,6 +6,7 @@ namespace SelectionStatement
     {
         static void Main(string[] args)
         {
+            /*
             if(args.Length == 0)
             {
                 Console.WriteLine("There are no argument.");
@@ -13,6 +14,19 @@ namespace SelectionStatement
             else
             {
                 Console.WriteLine("There is at least one argument.");
+            }
+            */
+
+            object o = "3";
+            int j = 4;
+
+            if(o is int i)
+            {
+                Console.WriteLine($"{i} x {j} = {i * j}");
+            }
+            else
+            {
+                Console.WriteLine("o is not an int so it cannot multiply!");
             }
         }
     }
