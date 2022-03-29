aafb46532fba4cdfc4cfeb25747d2c27d5e91e02
but-sergey <but-sergey@bk.ru>
2022-03-29 14:36:09 +0300

    switch case

--- Chapter03/SelectionStatement/Program.cs
+++ Chapter03/SelectionStatement/Program.cs
@@ -16,7 +16,7 @@ namespace SelectionStatement
                 Console.WriteLine("There is at least one argument.");
             }
             */
-
+            /*
             object o = "3";
             int j = 4;
 
@@ -27,6 +27,31 @@ namespace SelectionStatement
             else
             {
                 Console.WriteLine("o is not an int so it cannot multiply!");
+            }
+            */
+
+            A_label:
+            
+            var number = (new Random()).Next(1, 7);
+            Console.WriteLine($"My random number is {number}");
+            switch(number)
+            {
+                case 1:
+                    Console.WriteLine("One");
+                    break;
+                case 2:
+                    Console.WriteLine("Two");
+                    goto case 1;
+                case 3:
+                case 4:
+                    Console.WriteLine("Three or four");
+                    goto case 1;
+                case 5:
+                    System.Threading.Thread.Sleep(500);
+                    goto A_label;
+                default:
+                    Console.WriteLine("Default");
+                    break;
             }
         }
     }
