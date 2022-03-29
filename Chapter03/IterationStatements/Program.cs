e95a1df3bd3daf94a961b3766b7a6892b9265338
but-sergey <but-sergey@bk.ru>
2022-03-29 15:33:06 +0300

    While statement

--- Chapter03/IterationStatements/Program.cs
+++ Chapter03/IterationStatements/Program.cs
@@ -6,7 +6,13 @@ namespace IterationStatements
     {
         static void Main(string[] args)
         {
+            int x = 0;
 
+            while (x < 10)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
         }
     }
 }
