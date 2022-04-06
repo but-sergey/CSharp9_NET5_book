1828bae7c1c7c8489bea8c3d672ceb7867552993
but-sergey <but-sergey@bk.ru>
2022-04-06 14:37:17 +0300

    Simple exception handling

--- Chapter03/HandlingExceptions/Program.cs
+++ Chapter03/HandlingExceptions/Program.cs
@@ -6,7 +6,19 @@ namespace HandlingExceptions
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Before parsing");
+            Console.Write("What is your age? ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                int age = int.Parse(input);
+                Console.WriteLine($"You are {age} years old.");
+            }
+            catch
+            {
+            }
+            Console.WriteLine("After parsing");
         }
     }
 }
