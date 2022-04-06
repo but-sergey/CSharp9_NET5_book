2e8d192c6321239285edbeed611345135301afb8
but-sergey <but-sergey@bk.ru>
2022-04-06 13:48:25 +0300

    Simple casting

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -6,7 +6,21 @@ namespace CastingConverting
     {
         static void Main(string[] args)
         {
-            
+            int a = 10;
+            double b = a;
+            Console.WriteLine(b);
+
+            double c = 9.8;
+            int d = (int)c;
+            Console.WriteLine(d);
+
+            long e = 10;
+            int f = (int)e;
+            Console.WriteLine($"e is {e:N0} and f is {f:N0}");
+
+            e = long.MaxValue;
+            f = (int)e;
+            Console.WriteLine($"e is {e:N0} and f is {f:N0}");
         }
     }
 }
