2c20a1e02d2ae7f6c61e80a989b2f68294530740
but-sergey <but-sergey@bk.ru>
2022-04-06 14:07:13 +0300

    Simple Rounding

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -23,10 +23,17 @@ namespace CastingConverting
             f = (int)e;
             Console.WriteLine($"e is {e:N0} and f is {f:N0}");
             */
-
+            /*
             double g = 9.8;
             int h = Convert.ToInt32(g);
             Console.WriteLine($"g is {g} and h is {h}");
+            */
+
+            double[] doubles = new[] { 9.49, 9.5, 9.51, 10.49, 10.5, 10.51 };
+            foreach (double n in doubles)
+            {
+                Console.WriteLine($"ToInt({n}) is {Convert.ToInt32(n)}");
+            }
         }
     }
 }
