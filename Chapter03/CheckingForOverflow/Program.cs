41e0634564b98520ed460fcf154787a3e2e3fe51
but-sergey <but-sergey@bk.ru>
2022-05-01 10:26:19 +0300

    Overflow with checked and try..catch

--- Chapter03/CheckingForOverflow/Program.cs
+++ Chapter03/CheckingForOverflow/Program.cs
@@ -6,16 +6,23 @@ namespace CheckingForOverflow
     {
         static void Main(string[] args)
         {
-            checked
+            try
             {
-                int x = int.MaxValue - 1;
-                Console.WriteLine($"Initial value: {x}");
-                x++;
-                Console.WriteLine($"After incrementing: {x}");
-                x++;
-                Console.WriteLine($"After incrementing: {x}");
-                x++;
-                Console.WriteLine($"After incrementing: {x}");
+                checked
+                {
+                    int x = int.MaxValue - 1;
+                    Console.WriteLine($"Initial value: {x}");
+                    x++;
+                    Console.WriteLine($"After incrementing: {x}");
+                    x++;
+                    Console.WriteLine($"After incrementing: {x}");
+                    x++;
+                    Console.WriteLine($"After incrementing: {x}");
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The code overflowed but I caught the exception.");
             }
         }
     }
